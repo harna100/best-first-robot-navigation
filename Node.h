cc62370ac46fae81fb22fd46c9aa437539d93807
Paul Harnack <harna100@mail.chapman.edu>
2018-02-25 12:44:59 -0800

    Creates Node (struct) and NodeType (enum)

---
+++ Node.h
@@ -0,0 +1,15 @@
+//
+// Created by Paul-HP on 2/25/2018.
+//
+
+#ifndef AI_HW3_NODE_H
+#define AI_HW3_NODE_H
+
+#include "NodeType.h"
+struct Node {
+    int x;
+    int y;
+    NodeType nodeType;
+};
+
+#endif //AI_HW3_NODE_H
---
+++ NodeType.h
@@ -0,0 +1,15 @@
+//
+// Created by Paul-HP on 2/25/2018.
+//
+
+#ifndef AI_HW3_NODETYPE_H
+#define AI_HW3_NODETYPE_H
+
+enum NodeType {
+    Initial,
+    Empty,
+    Obstacle,
+    Goal
+};
+
+#endif //AI_HW3_NODETYPE_H
