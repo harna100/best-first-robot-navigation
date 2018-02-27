e54a897723a580e697b42e3e6d91e9776641c092
Paul Harnack <harna100@mail.chapman.edu>
2018-02-27 11:56:28 -0800

    Adds a list to fringe interface for exploring

--- Fringe.h
+++ Fringe.h
@@ -5,6 +5,7 @@
 #ifndef AI_HW3_FRINGE_H
 #define AI_HW3_FRINGE_H
 
+#include <list>
 #include "Node.h"
 
 virtual class Fringe {
@@ -12,6 +13,7 @@ virtual class Fringe {
         virtual void insertNode(Node toInsert) = 0;
         virtual Node* popNode() = 0;
     private:
+        std::list<Node*> nodesToExplore;
     protected:
 };
 
